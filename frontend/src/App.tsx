f3875a47bdd99569c59f6b4e0637b9f8619803ea
Marco <sgtmarcoaurelio@icloud.com>
2021-10-20 16:13:32 -0300

    :rocket: lesson two finalized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
- export function App() {
+import { useContext } from 'react';
+import styles from './App.module.scss';
+import { LoginBox } from './components/LoginBox';
+import { MessageList } from './components/MessageList';
+import { SendMessageForm } from './components/SendMessageForm';
+import { AuthContext } from './contexts/auth';
+
+export function App() {
+
+  const { user } = useContext(AuthContext);
+
   return (
-   <h1>Hello, Vite.</h1>
+    <main className={`${styles.contentWrapper} ${ !!user ? styles.contentSigned : '' }`}>
+      <MessageList />
+      { !!user ? <SendMessageForm /> : <LoginBox /> }
+    </main>
   )
 }
